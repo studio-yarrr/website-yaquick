6ec3e1f9bd0d0d14b9b405ffecf0026c2efea664
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-08-30 12:42:13 +0300

    last

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,8 +2,61 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 //= components/
+  class Menu {
+    constructor(menuElement, buttonElement) {
+      this.menu = typeof menuElement === "string" ? document.querySelector(menuElement) : menuElement
+      this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement
+      this.overlay = document.createElement('div')
+      this._init()
+    }
 
+    _init() {
+      document.body.appendChild(this.overlay)
+      this.overlay.classList.add('overlay')
 
+      console.log(this.menu, this.button)
+
+      this.overlay.addEventListener('click', this.toggleMenu.bind(this))
+      this.button.addEventListener('click', this.toggleMenu.bind(this))
+    }
+
+    toggleMenu() {
+      this.menu.classList.toggle('menu--open')
+      this.button.classList.toggle('menu-button--active')
+
+      if (this.isMenuOpen()) {
+        this.disableScroll()
+      } else {
+        this.enableScroll()
+      }
+    }
+
+    isMenuOpen() {
+      return this.menu.classList.contains('menu--open')
+    }
+
+    disableScroll() {
+        // Get the current page scroll position
+        const scrollTop = window.pageYOffset  || document.documentElement.scrollTop;
+        const scrollLeft = window.pageXOffset  || document.documentElement.scrollLeft;
+      
+            // if any scroll is attempted, set this to the previous value
+            window.onscroll = function() {
+                window.scrollTo(scrollLeft, scrollTop);
+            };
+    }
+
+    enableScroll() {
+      window.onscroll = function() {};
+    }
+  }
+
+  const menu = document.querySelector('.menu')
+  const menuButton = document.querySelector('.menu-button')
+
+  if (menu && menuButton) {
+    new Menu(menu, menuButton)
+  }
 })
 
 
