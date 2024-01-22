87b37fe385f6e2322c6b7f42f05e684ee0ae1832
hanomag <>
2024-01-22 18:03:09 +0300

    main

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 0);
   } else {
     document.documentElement.classList.add('fontsloaded')
-    xl.matches ? '' : initLetters() 
+    xl.matches ? '' : initLetters()
     initTitle()
   }
 
@@ -235,21 +235,21 @@ document.addEventListener("DOMContentLoaded", () => {
           let timeOffset = i * each / 1.45,
             startTime = dur / 2 + timeOffset,
             pathOffset = startTime / dur;
-              letterAnim.to(char, {
-                motionPath: {
-                  path: '#m',
-                  align: '#m',
-                  alignOrigin: [0.5, 0.5],
-                  autoRotate: true,
-                  start: pathOffset,
-                  end: 1 + pathOffset
-                },
-                stagger: 0.01,
-                immediateRender: false,
-                lazy: true,
-                duration: 20,
-                ease: "none",
-              }, 0);
+          letterAnim.to(char, {
+            motionPath: {
+              path: '#m',
+              align: '#m',
+              alignOrigin: [0.5, 0.5],
+              autoRotate: true,
+              start: pathOffset,
+              end: 1 + pathOffset
+            },
+            stagger: 0.01,
+            immediateRender: false,
+            lazy: true,
+            duration: 20,
+            ease: "none",
+          }, 0);
 
         });
       }
@@ -712,16 +712,16 @@ document.addEventListener("DOMContentLoaded", () => {
             } else {
               allNumbers.classList.remove('reached')
             }
-  
+
             if (reachEnd) {
               allnumb.classList.add('reached')
             } else {
               allnumb.classList.remove('reached')
             }
-  
+
             allNumbers.innerHTML = pad(sw.realIndex + 1, 2);
             allnumb.innerHTML = pad(numberOfSlides.length - 1, 2)
-  
+
             reachBeginning = false
             reachEnd = false
           }
@@ -824,6 +824,100 @@ document.addEventListener("DOMContentLoaded", () => {
 
           window.scrollTo({ top: y, behavior: 'smooth' });
         }
+      })
+    })
+  }
+
+  var x, i, j, l, ll, selElmnt, a, b, c;
+  /* Look for any elements with the class "custom-select": */
+  x = document.getElementsByClassName("custom-select");
+  l = x.length;
+  for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /* For each element, create a new DIV that will act as the selected item: */
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /* For each element, create a new DIV that will contain the option list: */
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+      /* For each option in the original select element,
+      create a new DIV that will act as an option item: */
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function (e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl, sel;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        sel = this.parentNode.previousSibling.classList.add('select-selected--active')
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+      /* When the select box is clicked, close any other select boxes,
+      and open/close the current select box: */
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
+
+  function closeAllSelect(elmnt) {
+    /* A function that will close all select boxes in the document,
+    except the current select box: */
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i)
+      } else {
+        y[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < xl; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide");
+      }
+    }
+  } 
+
+  /* If the user clicks anywhere outside the select box,
+  then close all select boxes: */
+  document.addEventListener("click", closeAllSelect);
+
+  const menubtns = document.querySelectorAll('.menu-subbtn')
+  if (menubtns.length) {
+    menubtns.forEach(el => {
+      el.addEventListener('click', function () {
+        if (xl.matches) {
+          event.preventDefault()
+        }
+        this.classList.toggle('opened')
       })
     })
   }
